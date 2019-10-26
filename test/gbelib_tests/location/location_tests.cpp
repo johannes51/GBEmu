dba284a28eed05ca9ed6cdad6a7c2cf4802bef5b
Johannes <j.greul@tu-bs.de>
2019-10-26 13:09:06 +0200

    added missing tests

--- test/gbelib_tests/location/location_tests.cpp
+++ test/gbelib_tests/location/location_tests.cpp
@@ -1,22 +1,32 @@
 #include "gtest/gtest.h"
 
+#include "debug/variablebyte.h"
 #include "location/location.h"
 #include "location/zerobyte.h"
 
-TEST(LocationTest, testDereference1)
+TEST(LocationTest, testGenerate1)
 {
   auto a = Location<uint8_t>::generate(std::make_unique<ZeroByte>());
   EXPECT_EQ(typeid(a.get()), typeid(uint8_t));
 }
 
-TEST(LocationTest, testDereference2)
+TEST(LocationTest, testGenerate2)
 {
   auto a = Location<uint16_t>::generate(std::make_unique<ZeroByte>(), std::make_unique<ZeroByte>());
   EXPECT_EQ(typeid(a.get()), typeid(uint16_t));
 }
 
-TEST(LocationTest, testDereference3)
+TEST(LocationTest, testGenerate3)
 {
   auto a = Location<uint16_t>::generate(std::make_unique<ZeroByte>());
   EXPECT_ANY_THROW(a.get());
+}
+
+TEST(LocationTest, testFuse)
+{
+  auto a = Location<uint8_t>::generate(std::make_unique<VariableByte>(0x3F));
+  auto b = Location<uint8_t>::generate(std::make_unique<VariableByte>(0x1E));
+  auto c = Location<uint8_t>::fuse(std::move(a), std::move(b));
+  EXPECT_EQ(typeid(uint16_t), typeid(c.get()));
+  EXPECT_EQ(0x1E3F, c.get());
 }
--- test/gbelib_tests/util/ops_tests.cpp
+++ test/gbelib_tests/util/ops_tests.cpp
@@ -4,7 +4,7 @@
 #include "location/rambyte.h"
 #include "util/ops.h"
 
-TEST(OpsTest, testIncrement)
+TEST(OpsTest, testIncrement8)
 {
   uint8_t buffer = 5;
   auto l = Location<uint8_t>::generate(std::make_unique<RamByte>(buffer));
@@ -12,4 +12,70 @@ TEST(OpsTest, testIncrement)
   ops::increment(std::move(l));
 
   EXPECT_EQ(6, buffer);
+}
+
+TEST(OpsTest, testIncrement16)
+{
+  uint8_t bufferL = 0xFF;
+  uint8_t bufferU = 0xAF;
+  auto l = Location<uint16_t>::generate(std::make_unique<RamByte>(bufferL), std::make_unique<RamByte>(bufferU));
+
+  ops::increment(std::move(l));
+
+  EXPECT_EQ(0x00, bufferL);
+  EXPECT_EQ(0xB0, bufferU);
+}
+
+TEST(OpsTest, testAdd8)
+{
+  uint8_t a = 5;
+  uint8_t b = 13;
+  auto lA = Location<uint8_t>::generate(std::make_unique<RamByte>(a));
+  auto lB = Location<uint8_t>::generate(std::make_unique<RamByte>(b));
+
+  ops::add(std::move(lA), std::move(lB));
+
+  EXPECT_EQ(5 + 13, a);
+}
+
+TEST(OpsTest, testAdd16)
+{
+  uint8_t aL = 0x35;
+  uint8_t bL = 0xA2;
+  uint8_t aU = 0x14;
+  uint8_t bU = 0xC7;
+  auto lA = Location<uint16_t>::generate(std::make_unique<RamByte>(aL), std::make_unique<RamByte>(aU));
+  auto lB = Location<uint16_t>::generate(std::make_unique<RamByte>(bL), std::make_unique<RamByte>(bU));
+
+  ops::add(std::move(lA), std::move(lB));
+
+  EXPECT_EQ(0x35 + 0xA2, aL);
+  EXPECT_EQ(0x14 + 0xC7, aU);
+}
+
+TEST(OpsTest, testLoad8)
+{
+  uint8_t dest = 0x3C;
+  uint8_t src = 0xF1;
+  auto d = Location<uint8_t>::generate(std::make_unique<RamByte>(dest));
+  auto s = Location<uint8_t>::generate(std::make_unique<RamByte>(src));
+
+  ops::load(std::move(d), std::move(s));
+
+  EXPECT_EQ(0xF1, dest);
+}
+
+TEST(OpsTest, testLoad16)
+{
+  uint8_t destL = 0x3C;
+  uint8_t destU = 0xB8;
+  uint8_t srcL = 0xF1;
+  uint8_t srcU = 0xC3;
+  auto d = Location<uint16_t>::generate(std::make_unique<RamByte>(destL), std::make_unique<RamByte>(destU));
+  auto s = Location<uint16_t>::generate(std::make_unique<RamByte>(srcL), std::make_unique<RamByte>(srcU));
+
+  ops::load(std::move(d), std::move(s));
+
+  EXPECT_EQ(0xF1, destL);
+  EXPECT_EQ(0xC3, destU);
 }
